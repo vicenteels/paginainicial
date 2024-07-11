1052aa17493c5019b5e2092bfff61e7e3d3e3331
vicenteels <seganfredov@gmail.com>
2024-07-11 11:31:39 -0300

    mudança

--- firebase-init.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBj53tTol5vOy2EoyCDYIzMBuIYUtOs93Y",
-    authDomain: "ecobag-af6f3.firebaseapp.com",
-    projectId: "ecobag-af6f3",
-    storageBucket: "ecobag-af6f3.appspot.com",
-    messagingSenderId: "76657489483",
-    appId: "1:76657489483:web:d8f051621c13e9e29abfce"
-  };
-  firebase.initialize(firebaseConfig);
