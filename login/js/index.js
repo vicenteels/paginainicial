c44498b8ea067c6e26c21370912abc878dda8586
vicenteels <seganfredov@gmail.com>
2024-08-15 09:27:52 -0300

    mensagens de erro nos botoes
    aparece mensagens de erro se os campos de email e senha forem preenchidos da maneira errada ou nem foram preenchidos

--- login/js/index.js
+++ login/js/index.js
@@ -1,9 +1,11 @@
-function validateFields(){
-    const emailValid = isEmailValid();
-    document.getElementById("recover-password-button").disabled = !emailValid;
+function onChangeEmail(){
+    toggleButtonsDisable();
+    toggleEmailErrors();
+}
 
-    const passwordValid = isPasswordValid();
-    document.getElementById("login-button").disabled = !emailValid || !passwordValid; //botao entrar fica desabilitado se o email não for valido ou se a senha não for valida
+function onChangePassword(){
+    togglePasswordErrors();
+    toggleButtonsDisable();
 }
 
 function isEmailValid(){
@@ -12,6 +14,38 @@ function isEmailValid(){
         return false;
     }
     return validateEmail(email);
+}
+
+function toggleEmailErrors(){
+    const email = document.getElementById('email').value;
+    if (!email){ //verifica se o campo do email esta vazio
+        document.getElementById('email-required-error').style.display = 'block'; //se esta vazio mostra mensagem de erro que tem q ter email
+    }else{ //se tiver algo escrito não mostra
+        document.getElementById('email-required-error').style.display = "none";
+    }
+
+    if (validateEmail(email)){ //chama a função de verificar se o email é valido e se receber dela um true não mostra nada
+        document.getElementById('email-invalid-error').style.display = "none"; 
+    }else{ //senão mostra mensagem de erro dizendo que o email é inválido
+        document.getElementById('email-invalid-error').style.display = "block";
+    }
+}
+
+function togglePasswordErrors(){
+    const password = document.getElementById("password").value;
+    if(!password){
+        document.getElementById("password-riquired-error").style.display = "block";
+    }else{
+        document.getElementById("password-riquired-error").style.display = "none";
+    }
+}
+
+function toggleButtonsDisable(){
+    const emailValid = isEmailValid();
+    document.getElementById("recover-password-button").disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById("login-button").disabled = !emailValid || !passwordValid; //botao entrar fica desabilitado se o email não for valido ou se a senha não for valida
 }
 
 function isPasswordValid(){
