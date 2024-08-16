fe9e612b149b251dd7a948d34203eb84bf927b77
vicenteels <seganfredov@gmail.com>
2024-08-15 21:20:25 -0300

    refatoração codigo

--- login/js/index.js
+++ login/js/index.js
@@ -9,7 +9,7 @@ function onChangePassword(){
 }
 
 function isEmailValid(){
-    const email = document.getElementById("email").value;
+    const email = form.email().value;
     if (!email){
         return false;
     }
@@ -17,45 +17,48 @@ function isEmailValid(){
 }
 
 function toggleEmailErrors(){
-    const email = document.getElementById('email').value;
-    if (!email){ //verifica se o campo do email esta vazio
-        document.getElementById('email-required-error').style.display = 'block'; //se esta vazio mostra mensagem de erro que tem q ter email
-    }else{ //se tiver algo escrito não mostra
-        document.getElementById('email-required-error').style.display = "none";
-    }
+    const email = form.email().value;
+    form.emailRequiredError().style.display = email ? "none" : "block";
+    //operador ternário
+    //verifica se o campo do email esta vazio
+    //se for verdadeiro, no caso se tiver algo escrito não mostra (segundo bloco do operador ternario)
+    //se esta vazio mostra mensagem de erro que tem q ter email
+    
+    form.emailInvalidError().style.display = validateEmail(email) ? "none" : "block";
+    //mesma coisa do de cima só que ele vai verificar se o email é valido
+    //por isso chamou a função validateEmail()
 
-    if (validateEmail(email)){ //chama a função de verificar se o email é valido e se receber dela um true não mostra nada
-        document.getElementById('email-invalid-error').style.display = "none"; 
-    }else{ //senão mostra mensagem de erro dizendo que o email é inválido
-        document.getElementById('email-invalid-error').style.display = "block";
-    }
 }
 
 function togglePasswordErrors(){
-    const password = document.getElementById("password").value;
-    if(!password){
-        document.getElementById("password-riquired-error").style.display = "block";
-    }else{
-        document.getElementById("password-riquired-error").style.display = "none";
-    }
+    const password = form.password().value;
+    form.passwordRequiredError().style.display = password ? "none" : "block";
+    //mesma coisa do de cima, se o campo password tiver valor ele não mostra o erro
+    //se não tiver nada escrito mostra o erro
 }
 
 function toggleButtonsDisable(){
     const emailValid = isEmailValid();
-    document.getElementById("recover-password-button").disabled = !emailValid;
+    form.recoverPassword().disabled = !emailValid;
 
     const passwordValid = isPasswordValid();
-    document.getElementById("login-button").disabled = !emailValid || !passwordValid; //botao entrar fica desabilitado se o email não for valido ou se a senha não for valida
+    form.loginButton().disabled = !emailValid || !passwordValid; //botao entrar fica desabilitado se o email não for valido ou se a senha não for valida
 }
 
 function isPasswordValid(){
-    const password = document.getElementById("password").value;
+    const password = form.password().value;
     if (!password){
         return false
     }
     return true;
 }
 
-function validateEmail(email){
-    return /\S+@\S+\.\S+/.test(email);
+const form = {
+    email: () => document.getElementById('email'),
+    emailInvalidError: () => document.getElementById('email-invalid-error'),
+    emailRequiredError: () => document.getElementById('email-required-error'),
+    password: () => document.getElementById('password'),
+    loginButton: () => document.getElementById('login-button'),
+    recoverPassword: () => document.getElementById('recover-password-button'),
+    passwordRequiredError: () => document.getElementById('password-required-error')
 }
