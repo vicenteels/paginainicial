7bd2684b998bd62e1055257ff720bbac1da77fa5
sofii4 <sofialopesdasilva00@gmail.com>
2024-09-16 18:54:46 -0300

    16/09 alterações

--- home/js/home.js
+++ home/js/home.js
@@ -5,3 +5,20 @@ function logout() {
         alert("Erro ao fazer logout")
     })
 }
+
+let lastScrollTop = 0;
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // Rolando para baixo - esconde o navbar
+        navbar.classList.add('hide');
+    } else {
+        // Rolando para cima - mostra o navbar
+        navbar.classList.remove('hide');
+    }
+
+    lastScrollTop = scrollTop;
+});
